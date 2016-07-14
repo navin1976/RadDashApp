b3de856b4d831e6641a1d5662549405e474eaf5b
Ambroise Laurent <ucabaur@ucl.ac.uk>
2016-07-14 12:28:19 +0100

    Functional routing with 2 pages, architecture layout

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
-/* eslint-disable no-console */
-console.log('Online!');
+/*babel-polyfill required because as of 14/07/16, many
+ES6 features needed for the project to work are not
+functional.*/
+
+import 'babel-polyfill';
+
+
+import React from 'react';
+import {render} from 'react-dom';
+import {Router, browserHistory} from 'react-router';
+
+import routes from './routes';
+//import './styles/styles.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+render(
+	<Router history={browserHistory} routes={routes}/>, 
+	document.getElementById('app')
+);
