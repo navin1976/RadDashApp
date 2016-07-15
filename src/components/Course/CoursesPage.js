46713ac1b833d3e37cd724bc284038eff1da3e4a
Ambroise Laurent <ucabaur@ucl.ac.uk>
2016-07-15 13:25:49 +0100

    Single store , dispatcher and reducer boilerplate

--- src/components/Course/CoursesPage.js
+++ src/components/Course/CoursesPage.js
@@ -1,10 +1,12 @@
 import React, {PropTypes} from 'react';
+import {connect} from 'react-redux';
+import * as courseActions from '../../actions/courseActions';
 
 class CoursesPage extends React.Component{
 	constructor(props,context){
 		super(props,context);
 		this.state = {
-			course:{title:null}
+			course:{title:""}
 		};
 		this.onTitleChange = this.onTitleChange.bind(this);
 		this.onClickSave = this.onClickSave.bind(this);
@@ -14,16 +16,22 @@ class CoursesPage extends React.Component{
 		const course = this.state.course;
 		course.title = event.target.value;
 		this.setState({course:course});
+
 	}
 
 	onClickSave(){
-		alert(`Saving ${this.state.course.title}`);
+		this.props.dispatch(courseActions.createCourse(this.state.course));
+	}
+
+	courseRow(course, index){
+		return <div key={index}>{course.title}</div>;
 	}
 
 	render(){
 		return(
 			<div>
 				<h1>Courses</h1>
+				{this.props.courses.map(this.courseRow)}
 				<h2>Add course</h2>
 				<input type="text" onChange={this.onTitleChange} value={this.state.course.title} />
 				<input type="submit" onClick={this.onClickSave} value="Save" />
@@ -32,4 +40,15 @@ class CoursesPage extends React.Component{
 	}
 }
 
-export default CoursesPage;
+function mapStateToProps(state,ownProps){
+	return{
+		courses: state.courses
+	};
+}
+
+CoursesPage.propTypes = {
+	dispatch: PropTypes.func.isRequired,
+	courses: PropTypes.array.isRequired
+};
+
+export default connect(mapStateToProps)(CoursesPage);
