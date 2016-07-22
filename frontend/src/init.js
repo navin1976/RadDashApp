04245bd60cc6967c3343f75df590b11160a28750
laurenta2 <ucabaur@ucl.ac.uk>
2016-07-22 12:48:52 +0100

    Working

--- frontend/src/init.js
+++ frontend/src/init.js
@@ -3,17 +3,17 @@ export default function getInitialData(){
 		courses:[{title:"COMPGC02"},{title:"COMPGC03"}],
 		layout:[
 			{
-				l:{i: '1', x: 0, y: 0, w: 3, h: 8},
+				l:{i: '1', x: 0, y: 0, w: 4, h: 8},
 				c:2,
 				n:"Dashboard 1"
 			},
 			{
-				l:{i: '2', x: 1, y: 0, w: 3, h: 2},
+				l:{i: '2', x: 4, y: 0, w: 4, h: 8},
 				c:2,
 				n:"Dashboard 2"
 			},
 			{
-				l:{i: '3', x: 4, y: 0, w: 1, h: 2},
+				l:{i: '3', x: 0, y: 8, w: 4, h: 8},
 				c:6,
 				n:"Dashboard 3"
 			}
