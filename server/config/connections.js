60a8f7093e29cb23b91f7ef7836d95f51e2a73f2
Melinda <melinda.fekete@msn.com>
2016-07-26 21:36:21 +0100

    configuration

--- server/config/connections.js
+++ server/config/connections.js
@@ -40,13 +40,13 @@ module.exports.connections = {
   * Run: npm install sails-mysql                                             *
   *                                                                          *
   ***************************************************************************/
-  // someMysqlServer: {
-  //   adapter: 'sails-mysql',
-  //   host: 'YOUR_MYSQL_SERVER_HOSTNAME_OR_IP_ADDRESS',
-  //   user: 'YOUR_MYSQL_USER', //optional
+   someMysqlServer: {
+     adapter: 'sails-mysql',
+     host: '127.0.0.1',
+     user: 'root', //optional
   //   password: 'YOUR_MYSQL_PASSWORD', //optional
-  //   database: 'YOUR_MYSQL_DB' //optional
-  // },
+     database: 'radiology_dashboard' //optional
+   },
 
   /***************************************************************************
   *                                                                          *
