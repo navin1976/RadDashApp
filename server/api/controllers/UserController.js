df88af86488c44f36499dd3c06e028fcef177534
Melinda <melinda.fekete@msn.com>
2016-08-02 09:32:07 +0100

    FindOne Customer

--- server/api/controllers/UserController.js
+++ server/api/controllers/UserController.js
@@ -8,5 +8,25 @@ module.exports = {
       res.type('application/json');
       return res.send(JSON.stringify(records, null, 2));
     });
+  },
+
+  findOne: function (res, req) {
+    console.log(req.params);
+    id = req.params.userId;
+    User.findOne({id: userId}).exec(function(error, records) {
+      if (error) {
+        // handle error here- e.g. `res.serverError(err);`
+        res.status(404);
+        return res.send();
+      }
+      if (!id) {
+        return res.notFound('Could not find Finn, sorry.');
+      }
+      res.status(205);
+      res.type('application/json');
+      return res.send(JSON.stringify(records, null, 2));
+    });
   }
 }
+
+
