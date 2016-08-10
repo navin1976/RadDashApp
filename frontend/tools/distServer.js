97f543147d3806ed4a73a6adcdc94dc4784d51cb
laurenta2 <ucabaur@ucl.ac.uk>
2016-08-10 21:53:14 +0200

    Compression added

--- frontend/tools/distServer.js
+++ frontend/tools/distServer.js
@@ -1,11 +1,12 @@
 import open from 'open';
 import path from 'path';
 import express from 'express';
-
+import compression from 'compression';
 /*eslint-disable no-console*/
+
 const port = 3000;
 const app = express();
-
+app.use(compression());
 app.use(express.static('dist'));
 app.get('*', function(req,res){
 	res.sendFile(path.join(__dirname,'../dist/index.html'));
