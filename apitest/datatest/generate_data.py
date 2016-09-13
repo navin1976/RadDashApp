fc84a24991d7f5c4e2e005ae1fe169d8bef71372
Melinda <melinda.fekete@msn.com>
2016-09-14 00:21:13 +0100

    improve dummy data

--- apitest/datatest/generate_data.py
+++ apitest/datatest/generate_data.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-
+import csv
 testDatasourcePath = 'datatest/datasource.csv'
 testForbiddenDatasourcePath = 'datatest/forbidden_datasource.csv'
 
@@ -12,13 +12,17 @@ freq = '5H'
 print('Generating data from ' + startDate + ' to ' + endDate + ' with a freq of ' + freq + '... ')
 idx = pd.date_range(startDate, endDate, freq=freq)
 datasource = pd.DataFrame([], index=idx)
-datasource['dimension1'] = np.random.randint(5, size=len(idx))
-datasource['dimension2'] = np.random.randint(3, size=len(idx))
+remove_n = int(0.3*len(idx))
+drop_indices = np.random.choice(datasource.index, remove_n, replace=False)
+datasource = datasource.drop(drop_indices)
+datasource['scannerID'] = np.random.randint(5, size=len(datasource.index))
+datasource['roomID'] = np.random.randint(3, size=len(datasource.index))
+
 
 forbiddenDatasource = pd.DataFrame([], index=idx)
 forbiddenDatasource['dimension1'] = np.random.randint(5, size=len(idx))
 
-datasource.to_csv(testDatasourcePath, index_label='date')
-forbiddenDatasource.to_csv(testForbiddenDatasourcePath, index_label='date')
+datasource.to_csv(testDatasourcePath, index_label='date', header=None, quoting=csv.QUOTE_NONE)
+forbiddenDatasource.to_csv(testForbiddenDatasourcePath, index_label='date',header=None, quoting=csv.QUOTE_NONE)
 
 print('Data generated.')
